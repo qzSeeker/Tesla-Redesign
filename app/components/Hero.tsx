8c22bc0d381b2c86a3983764366b1bf5a1a2d210
Arpit Yadav <arpityadavay2005@gmail.com>
2025-10-31 19:51:26 +0530

    fig bug

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 
 function Hero() {
-    const [hoveredButton, setHoveredButton] = useState(null);
+    const [hoveredButton, setHoveredButton] = useState<'explore' | 'learn' | null>(null);
 
     return (
         <div className='relative'>
